0df8d7b7f210662055b4a521086083da10bc5072
RestoGrow Master <restogrow@master.com>
2026-06-30 22:15:30 +0530

    Fix Vercel serverless crash by removing compression middleware

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const path = require('path');
-const compression = require('compression');
 const db = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(compression());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
